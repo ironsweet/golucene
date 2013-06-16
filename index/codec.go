6f83aa22dd1a1d8c8e07c254e0e862b3b37c682b
Yi Yan Zhou <balzaczyy@gmail.com>
2013-06-16 21:46:12 +0800

    add checkHeader method

--- index/codec.go
+++ index/codec.go
@@ -9,6 +9,20 @@ const (
 	CODEC_MAGIC = 0x3fd76c17
 )
 
+func CheckHeader(in *store.DataInput, codec string, minVersion, maxVersion int) (v int, err error) {
+	// Safety to guard against reading a bogus string:
+	actualHeader, err := in.ReadInt()
+	if err != nil {
+		return 0, err
+	}
+	if actualHeader != CODEC_MAGIC {
+		return 0, &CorruptIndexError{fmt.Sprintf(
+			"codec header mismatch: actual header=%v vs expected header=%v (resource: %v)",
+			actualHeader, CODEC_MAGIC, in)}
+	}
+	return CheckHeaderNoMagic(in, codec, minVersion, maxVersion)
+}
+
 func CheckHeaderNoMagic(in *store.DataInput, codec string, minVersion, maxVersion int) (v int, err error) {
 	actualCodec, err := in.ReadString()
 	if err != nil {
