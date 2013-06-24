f306def77b8d2c2e1df374092820d2d36f5d6077
Yi Yan Zhou <balzaczyy@gmail.com>
2013-06-24 23:41:32 +0800

    add ForUtil and PackedInts code

---
+++ util/packed.go
@@ -0,0 +1,56 @@
+package util
+
+import (
+	"fmt"
+	"math"
+)
+
+const (
+	PACKED_VERSION_START        = 0
+	PACKED_VERSION_BYTE_ALIGNED = 1
+	PACKED_VERSION_CURRENT      = PACKED_VERSION_BYTE_ALIGNED
+)
+
+func CheckVersion(version int) {
+	if version < PACKED_VERSION_START {
+		panic(fmt.Sprintf("Version is too old, should be at least %v (got %v)", PACKED_VERSION_START, version))
+	} else if version > PACKED_VERSION_CURRENT {
+		panic(fmt.Sprintf("Version is too new, should be at most %v (got %v)", PACKED_VERSION_CURRENT, version))
+	}
+}
+
+type PackedFormat int
+
+func (f PackedFormat) byteCount(packedIntsVersion, valueCount, bitsPerValue int) int64 {
+	switch int(f) {
+	case 0:
+		if packedIntsVersion < PACKED_VERSION_BYTE_ALIGNED {
+			return 8 * int64(math.Ceil(float64(valueCount)*float64(bitsPerValue)/64))
+		}
+		return int64(math.Ceil(float64(valueCount) * float64(bitsPerValue) / 8))
+	}
+	// assert bitsPerValue >= 0 && bitsPerValue <= 64
+	// assume long-aligned
+	return 8 * int64(f.longCount(packedIntsVersion, valueCount, bitsPerValue))
+}
+
+func (f PackedFormat) longCount(packedIntsVersion, valueCount, bitsPerValue int) int {
+	switch int(f) {
+	case 1:
+		valuesPerBlock := 64 / bitsPerValue
+		return int(math.Ceil(float64(valueCount) / float64(valuesPerBlock)))
+	}
+	// assert bitsPerValue >= 0 && bitsPerValue <= 64
+	ans := f.byteCount(packedIntsVersion, valueCount, bitsPerValue)
+	// assert ans < 8 * math.MaxInt32()
+	if ans%8 == 0 {
+		return int(ans / 8)
+	}
+	return int(ans/8) + 1
+}
+
+type PackedIntsEncoder interface {
+}
+
+type PackedIntsDecoder interface {
+}
