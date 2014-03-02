b2d27ae64605cc34b74c4bc03075753f1acc1d65
Yi Yan Zhou <balzaczyy@gmail.com>
2014-03-02 22:07:25 +0800

    implement DocumentsWriterFlushQueue.hasTickets()

--- core/index/flushQueue.go
+++ core/index/flushQueue.go
@@ -1,8 +1,14 @@
 package index
 
+import (
+	"fmt"
+	"sync/atomic"
+)
+
 // index/DocumentsWriterFlushQueue.java
 
 type DocumentsWriterFlushQueue struct {
+	ticketCount int32
 }
 
 func newDocumentsWriterFlushQueue() *DocumentsWriterFlushQueue {
@@ -14,7 +20,15 @@ func (fq *DocumentsWriterFlushQueue) addDeletes(deleteQueue *DocumentsWriterDele
 }
 
 func (fq *DocumentsWriterFlushQueue) hasTickets() bool {
-	panic("not implemented yet")
+	n := atomic.LoadInt32(&fq.ticketCount)
+	assertn(n >= 0, "ticketCount should be >= 0 but was: ", n)
+	return n != 0
+}
+
+func assertn(ok bool, msg string, args ...interface{}) {
+	if !ok {
+		panic(fmt.Sprintf(msg, args...))
+	}
 }
 
 func (fq *DocumentsWriterFlushQueue) forcePurge(writer *IndexWriter) (int, error) {
