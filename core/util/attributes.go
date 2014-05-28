35db9b26d5ea54d99a885232004c467e3a7a39fb
Yi Yan Zhou <balzaczyy@gmail.com>
2014-05-28 22:51:59 +0800

    implement AttributeSource ctor

--- core/util/attributes.go
+++ core/util/attributes.go
@@ -19,6 +19,10 @@ source of usually streamed ojects, e.g. a TokenStream.
 */
 type AttributeImpl struct{}
 
+func (v *AttributeImpl) Clone() *AttributeImpl {
+	panic("not implemented yet")
+}
+
 // util/AttributeSource.java
 
 /* An AttributeFactory creates instances of AttributeImpls. */
@@ -38,6 +42,21 @@ var DEFAULT_ATTRIBUTE_FACTORY = &DefaultAttributeFactory{
 	make(map[reflect.Type]Attribute),
 }
 
+/* This class holds the state of an AttributeSource */
+type AttributeState struct {
+	value *AttributeImpl
+	next  *AttributeState
+}
+
+func (s *AttributeState) Clone() *AttributeState {
+	ans := new(AttributeState)
+	ans.value = s.value.Clone()
+	if s.next != nil {
+		ans.next = s.next.Clone()
+	}
+	return ans
+}
+
 /*
 An AttributeSource contains a list of different AttributeImpls, and
 methods to add and get them. There can only be a single instance of
@@ -48,6 +67,10 @@ type is already present. If yes, it returns the instance, otherwise
 it creates a new instance and returns it.
 */
 type AttributeSource struct {
+	attributes     map[reflect.Type]Attribute
+	attributeImpls map[reflect.Type]*AttributeImpl
+	currentState   []*AttributeState
+	factory        AttributeFactory
 }
 
 /* An AttributeSource using the default attribute factory */
@@ -57,5 +80,13 @@ func NewAttributeSource() *AttributeSource {
 
 /* An AttributeSource using the supplied AttributeFactory for creating new Attribute instance. */
 func NewAttributeSourceWith(factory AttributeFactory) *AttributeSource {
-	panic("not implemented yet")
+	// Note that Lucene Java use LinkedHashMap to keep insert order.
+	// But it's used by Solr only and GoLucene doesn't have plan to
+	// port GoSolr. So we use plain map here.
+	return &AttributeSource{
+		attributes:     make(map[reflect.Type]Attribute),
+		attributeImpls: make(map[reflect.Type]*AttributeImpl),
+		currentState:   make([]*AttributeState, 1),
+		factory:        factory,
+	}
 }
