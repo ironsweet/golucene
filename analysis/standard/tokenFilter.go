ef8d02ffc1b340c9e0fe7f7a349405b5ca150557
Yi Yan Zhou <balzaczyy@gmail.com>
2014-05-31 12:44:10 +0800

    implement StandardFilter.incrementToken()

--- analysis/standard/tokenFilter.go
+++ analysis/standard/tokenFilter.go
@@ -9,15 +9,24 @@ import (
 type StandardFilter struct {
 	*TokenFilter
 	matchVersion util.Version
+	input        TokenStream
 }
 
 func newStandardFilter(matchVersion util.Version, in TokenStream) *StandardFilter {
 	return &StandardFilter{
 		TokenFilter:  NewTokenFilter(in),
 		matchVersion: matchVersion,
+		input:        in,
 	}
 }
 
 func (f *StandardFilter) IncrementToken() (bool, error) {
-	panic("not implemented yet")
+	assert(f.matchVersion.OnOrAfter(util.VERSION_31))
+	return f.input.IncrementToken()
+}
+
+func assert(ok bool) {
+	if !ok {
+		panic("assert fail")
+	}
 }
--- core/util/version.go
+++ core/util/version.go
@@ -3,8 +3,14 @@ package util
 type Version int
 
 const (
+	// Match settings and bugs in Lucene's 3.1 release.
+	VERSION_31 = Version(31)
 	// Match settings and bugs in Lucene's 4.5 release.
 	// Use this to get the latest and greatest settings, bug fixes, etc.
 	// It's the only supported version for golucene now.
 	VERSION_45 = Version(45)
 )
+
+func (v Version) OnOrAfter(other Version) bool {
+	return int(v) >= int(other)
+}
