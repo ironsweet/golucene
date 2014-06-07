4f98d45485b99e37063a82744212af343fd6a217
Yi Yan Zhou <balzaczyy@gmail.com>
2014-06-07 19:45:32 +0800

    implement FloatToByte315()

--- core/util/smallFloat.go
+++ core/util/smallFloat.go
@@ -1,5 +1,9 @@
 package util
 
+import (
+	"math"
+)
+
 // util/SmallFloat.java
 
 /*
@@ -8,6 +12,17 @@ smallest non-zero value = 5.820766E-10
 largest value = 7.5161928E9
 epsilon = 0.125
 */
-func FloatToByte315(f float32) byte {
-	panic("not implemented yet")
+func FloatToByte315(f float32) int8 {
+	bits := math.Float32bits(f)
+	smallfloat := bits >> (24 - 3)
+	if smallfloat <= ((63 - 15) << 3) {
+		if bits <= 0 {
+			return 0
+		}
+		return 1
+	}
+	if smallfloat >= ((63-15)<<3)+0x100 {
+		return -1
+	}
+	return int8(smallfloat - ((63 - 15) << 3))
 }
