65b0e8e11db688a0e71272f31102a4f591810252
Yi Yan Zhou <balzaczyy@gmail.com>
2014-06-22 00:12:30 +0800

    implement TermStats ctor

--- core/codec/termStats.go
+++ core/codec/termStats.go
@@ -2,8 +2,13 @@ package codec
 
 /* Holder for per-term statistics. */
 type TermStats struct {
+	// How many documents have at least one occurrence of this term.
+	docFreq int
+	// Total number of times this term occurs across all documents in
+	// the field.
+	totalTermFreq int64
 }
 
 func NewTermStats(docFreq int, totalTermFreq int64) *TermStats {
-	panic("not implemented yet")
+	return &TermStats{docFreq, totalTermFreq}
 }
