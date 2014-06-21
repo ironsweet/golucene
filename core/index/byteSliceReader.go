1c08aa12d56387338e7145cc0f3551f62056e5ae
Yi Yan Zhou <balzaczyy@gmail.com>
2014-06-21 20:35:20 +0800

    implement ByteSliceReader.init()

--- core/index/byteSliceReader.go
+++ core/index/byteSliceReader.go
@@ -12,10 +12,39 @@ the next slice and then jump to it.
 */
 type ByteSliceReader struct {
 	*util.DataInputImpl
+	pool         *util.ByteBlockPool
+	bufferUpto   int
+	buffer       []byte
+	upto         int
+	limit        int
+	level        int
+	bufferOffset int
+
+	endIndex int
 }
 
 func (r *ByteSliceReader) init(pool *util.ByteBlockPool, startIndex, endIndex int) {
-	panic("not implemented yet")
+	assert(endIndex-startIndex >= 0)
+	assert(startIndex >= 0)
+	assert(endIndex >= 0)
+
+	r.pool = pool
+	r.endIndex = endIndex
+
+	r.level = 0
+	r.bufferUpto = startIndex / util.BYTE_BLOCK_SIZE
+	r.bufferOffset = r.bufferUpto * util.BYTE_BLOCK_SIZE
+	r.buffer = pool.Buffers[r.bufferUpto]
+	r.upto = startIndex & util.BYTE_BLOCK_MASK
+
+	firstSize := util.LEVEL_SIZE_ARRAY[0]
+
+	if startIndex+firstSize >= endIndex {
+		// there is only this one slice to read
+		r.limit = endIndex & util.BYTE_BLOCK_MASK
+	} else {
+		r.limit = r.upto + firstSize - 4
+	}
 }
 
 func (r *ByteSliceReader) eof() bool {
