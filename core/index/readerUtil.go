ac2b18c795a4a250bdfdd47ed2c8687ce74b8ceb
Yi Yan Zhou <balzaczyy@gmail.com>
2014-06-30 14:27:25 +0800

    implement ReaderUtil.subIndex()

--- core/index/readerUtil.go
+++ core/index/readerUtil.go
@@ -8,5 +8,22 @@ to construct this searcher/reader.
 */
 func SubIndex(n int, leaves []*AtomicReaderContext) int {
 	// find searcher/reader for doc n:
-	panic("not implemented yet")
+	size := len(leaves)
+	lo, hi := 0, size-1
+	// bi-search starts array, for first element less thann, return its index
+	for lo <= hi {
+		mid := (lo + hi) >> 1
+		midValue := leaves[mid].DocBase
+		if n < midValue {
+			hi = mid - 1
+		} else if n > midValue {
+			lo = mid + 1
+		} else { // found a match
+			for mid+1 < size && leaves[mid+1].DocBase == midValue {
+				mid++ // scan to last match
+			}
+			return mid
+		}
+	}
+	return hi
 }
