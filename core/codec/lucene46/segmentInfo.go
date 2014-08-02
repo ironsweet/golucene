78f43d7af001baa984c4a12886b18920f6a67cbb
Yi Yan Zhou <balzaczyy@gmail.com>
2014-08-02 23:06:27 +0800

    implement Lucene46SegmentInfoReader

--- core/codec/lucene46/segmentInfo.go
+++ core/codec/lucene46/segmentInfo.go
@@ -1,6 +1,8 @@
 package lucene46
 
 import (
+	"errors"
+	"fmt"
 	"github.com/balzaczyy/golucene/core/codec"
 	. "github.com/balzaczyy/golucene/core/codec/spi"
 	. "github.com/balzaczyy/golucene/core/index/model"
@@ -14,6 +16,7 @@ const (
 	// File extension used to store SegmentInfo.
 	SI_EXTENSION        = "si"
 	SI_CODEC_NAME       = "Lucene46SegmentInfo"
+	SI_VERSION_START    = 0
 	SI_VERSION_CHECKSUM = 1
 	SI_VERSION_CURRENT  = SI_VERSION_CHECKSUM
 )
@@ -26,11 +29,80 @@ func NewLucene46SegmentInfoFormat() *Lucene46SegmentInfoFormat {
 }
 
 func (f *Lucene46SegmentInfoFormat) SegmentInfoReader() SegmentInfoReader {
-	panic("not implemented yet")
+	return f
 }
 
 func (f *Lucene46SegmentInfoFormat) SegmentInfoWriter() SegmentInfoWriter {
 	return f
+}
+
+func (f *Lucene46SegmentInfoFormat) Read(dir store.Directory,
+	segName string, ctx store.IOContext) (si *SegmentInfo, err error) {
+
+	filename := util.SegmentFileName(segName, "", SI_EXTENSION)
+	var input store.ChecksumIndexInput
+	if input, err = dir.OpenChecksumInput(filename, ctx); err != nil {
+		return
+	}
+
+	var success = false
+	defer func() {
+		if !success {
+			util.CloseWhileSuppressingError(input)
+		} else {
+			err = input.Close()
+		}
+	}()
+
+	var codecVersion int
+	if codecVersion, err = asInt(codec.CheckHeader(input, SI_CODEC_NAME, SI_VERSION_START, SI_VERSION_CURRENT)); err != nil {
+		return
+	}
+	var version string
+	if version, err = input.ReadString(); err != nil {
+		return
+	}
+	var docCount int
+	if docCount, err = asInt(input.ReadInt()); err != nil {
+		return
+	} else if docCount < 0 {
+		return nil, errors.New(fmt.Sprintf("invalid docCount: %v (resource=%v)", docCount, input))
+	}
+	var b byte
+	if b, err = input.ReadByte(); err != nil {
+		return
+	}
+	var isCompoundFile = b == YES
+	var diagnostics map[string]string
+	if diagnostics, err = input.ReadStringStringMap(); err != nil {
+		return
+	}
+	var files map[string]bool
+	if files, err = input.ReadStringSet(); err != nil {
+		return
+	}
+
+	if codecVersion >= SI_VERSION_CHECKSUM {
+		_, err = codec.CheckFooter(input)
+	} else {
+		err = codec.CheckEOF(input)
+	}
+	if err != nil {
+		return
+	}
+
+	si = NewSegmentInfo(dir, version, segName, docCount, isCompoundFile, nil, diagnostics)
+	si.SetFiles(files)
+
+	success = true
+	return si, nil
+}
+
+func asInt(n int32, err error) (int, error) {
+	if err != nil {
+		return 0, err
+	}
+	return int(n), nil
 }
 
 func (f *Lucene46SegmentInfoFormat) Write(dir store.Directory, si *SegmentInfo, fis FieldInfos, ctx store.IOContext) error {
