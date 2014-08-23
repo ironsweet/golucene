e7d6011194596601836f44cc947c43f87c5a87fc
Yi Yan Zhou <balzaczyy@gmail.com>
2014-08-23 23:09:03 +0800

    implement Lucene49NormsFormat.normsConsumer()

--- core/codec/lucene49/norms.go
+++ core/codec/lucene49/norms.go
@@ -1,6 +1,7 @@
 package lucene49
 
 import (
+	"fmt"
 	. "github.com/balzaczyy/golucene/core/codec/spi"
 	. "github.com/balzaczyy/golucene/core/index/model"
 )
@@ -9,9 +10,30 @@ type Lucene49NormsFormat struct {
 }
 
 func (f *Lucene49NormsFormat) NormsConsumer(state *SegmentWriteState) (w DocValuesConsumer, err error) {
-	panic("not implemented yet")
+	return newLucene49NormsConsumer(state, DATA_CODEC, DATA_EXTENSION, METADATA_CODEC, METADATA_EXTENSION)
 }
 
 func (f *Lucene49NormsFormat) NormsProducer(state SegmentReadState) (r DocValuesProducer, err error) {
 	panic("not implemented yet")
+}
+
+const (
+	DATA_CODEC         = "Lucene49NormsData"
+	DATA_EXTENSION     = "nvd"
+	METADATA_CODEC     = "Lucene49NormsMetadata"
+	METADATA_EXTENSION = "nvm"
+	VERSION_START      = 0
+	VERSION_CURRENT    = VERSION_START
+)
+
+func assert(ok bool) {
+	if !ok {
+		panic("assert fail")
+	}
+}
+
+func assert2(ok bool, msg string, args ...interface{}) {
+	if !ok {
+		panic(fmt.Sprintf(msg, args...))
+	}
 }
---
+++ core/codec/lucene49/normsConsumer.go
@@ -0,0 +1,168 @@
+package lucene49
+
+import (
+	"github.com/balzaczyy/golucene/core/codec"
+	. "github.com/balzaczyy/golucene/core/index/model"
+	"github.com/balzaczyy/golucene/core/store"
+	"github.com/balzaczyy/golucene/core/util"
+	"github.com/balzaczyy/golucene/core/util/packed"
+	"math"
+)
+
+// lucene49/Lucene49NormsConsumer.java
+
+const CONST_COMPRESSED = 2
+
+type NormsConsumer struct {
+	data, meta store.IndexOutput
+	maxDoc     int
+}
+
+func newLucene49NormsConsumer(state *SegmentWriteState,
+	dataCodec, dataExtension, metaCodec, metaExtension string) (nc *NormsConsumer, err error) {
+
+	assert(packed.PackedFormat(packed.PACKED_SINGLE_BLOCK).IsSupported(1))
+	assert(packed.PackedFormat(packed.PACKED_SINGLE_BLOCK).IsSupported(2))
+	assert(packed.PackedFormat(packed.PACKED_SINGLE_BLOCK).IsSupported(4))
+
+	nc = &NormsConsumer{maxDoc: state.SegmentInfo.DocCount()}
+	var success = false
+	defer func() {
+		if !success {
+			util.CloseWhileSuppressingError(nc)
+		}
+	}()
+
+	dataName := util.SegmentFileName(state.SegmentInfo.Name, state.SegmentSuffix, dataExtension)
+	if nc.data, err = state.Directory.CreateOutput(dataName, state.Context); err != nil {
+		return nil, err
+	}
+	if err = codec.WriteHeader(nc.data, dataCodec, VERSION_CURRENT); err != nil {
+		return nil, err
+	}
+	metaName := util.SegmentFileName(state.SegmentInfo.Name, state.SegmentSuffix, metaExtension)
+	if nc.meta, err = state.Directory.CreateOutput(metaName, state.Context); err != nil {
+		return nil, err
+	}
+	if err = codec.WriteHeader(nc.meta, metaCodec, VERSION_CURRENT); err != nil {
+		return nil, err
+	}
+	success = true
+	return nc, nil
+}
+
+func (nc *NormsConsumer) AddNumericField(field *FieldInfo,
+	f func() (interface{}, bool)) (err error) {
+
+	if err = nc.meta.WriteVInt(field.Number); err != nil {
+		return
+	}
+	minValue, maxValue := int64(math.MaxInt64), int64(math.MinInt64)
+	// TODO: more efficient?
+	uniqueValues := make(map[int64]bool)
+
+	count := int64(0)
+	for {
+		nv, ok := f()
+		if !ok {
+			break
+		}
+		assert2(nv != nil, "illegal norms data for field %v, got null for value: %v", field.Name, count)
+		v := nv.(int64)
+
+		if v < minValue {
+			minValue = v
+		}
+		if v > maxValue {
+			maxValue = v
+		}
+
+		if uniqueValues != nil {
+			_, ok := uniqueValues[v]
+			uniqueValues[v] = true
+			if !ok && len(uniqueValues) > 256 {
+				uniqueValues = nil
+			}
+		}
+
+		count++
+	}
+	assert2(count == int64(nc.maxDoc),
+		"illegal norms data for field %v, expected %v values, got %v",
+		field.Name, nc.maxDoc, count)
+
+	if len(uniqueValues) == 1 {
+		// 0 bpv
+		if err = nc.meta.WriteByte(CONST_COMPRESSED); err != nil {
+			return
+		}
+		if err = nc.meta.WriteLong(minValue); err != nil {
+			return
+		}
+	} else if len(uniqueValues) > 0 {
+		// small number of unique values; this is the typical case:
+		// we only use bpv=1,2,4,8
+		// format := packed.PACKED_SINGLE_BLOCK
+		bitsPerValue := packed.BitsRequired(int64(len(uniqueValues)) - 1)
+		if bitsPerValue == 3 {
+			bitsPerValue = 4
+		} else if bitsPerValue > 4 {
+			bitsPerValue = 8
+		}
+
+		panic("not implemented yet")
+		// formatAndBits := packed.FastestFormatAndBits(nc.maxDoc, bitsPerValue, nc.acceptableOverheadRatio)
+		// if formatAndBits.BitsPerValue == 8 && minValue >= math.MinInt8 && maxValue <= math.MaxInt8 {
+		// 	err = nc.meta.WriteByte(UNCOMPRESSED) // uncompressed
+		// 	if err != nil {
+		// 		return err
+		// 	}
+		// 	for {
+		// 		nv, ok := f()
+		// 		if !ok {
+		// 			break
+		// 		}
+		// 		n := byte(0)
+		// 		if nv != nil {
+		// 			n = byte(nv.(int64))
+		// 		}
+		// 		err = nc.data.WriteByte(byte(n))
+		// 		if err != nil {
+		// 			return err
+		// 		}
+		// 	}
+		// } else {
+		// 	panic("not implemented yet")
+		// }
+	} else {
+		panic("not implemented yet")
+	}
+	return nil
+}
+
+func (nc *NormsConsumer) Close() (err error) {
+	var success = false
+	defer func() {
+		if success {
+			err = util.Close(nc.data, nc.meta)
+		} else {
+			util.CloseWhileSuppressingError(nc.data, nc.meta)
+		}
+	}()
+
+	if nc.meta != nil {
+		if err = nc.meta.WriteVInt(-1); err != nil { // write EOF marker
+			return
+		}
+		if err = codec.WriteFooter(nc.meta); err != nil { // write checksum
+			return
+		}
+	}
+	if nc.data != nil {
+		if err = codec.WriteFooter(nc.data); err != nil { // write checksum
+			return
+		}
+	}
+	success = true
+	return nil
+}
