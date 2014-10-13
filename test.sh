a34d6979044ce05f29ad9f163bef12477717dbd6
Yi Yan Zhou <balzaczyy@gmail.com>
2014-10-14 00:08:16 +0800

    enable goveralls

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-export tests_codec=Lucene45
+export tests_codec=Lucene49
 
 go test github.com/balzaczyy/golucene/core/util
 go test github.com/balzaczyy/golucene/core/util/automaton
@@ -16,4 +16,4 @@ go test github.com/balzaczyy/golucene/core/codec/blocktree
 go test github.com/balzaczyy/golucene/core/codec/lucene42
 go test github.com/balzaczyy/golucene/core/index
 go test github.com/balzaczyy/golucene/core/search
-# go test github.com/balzaczyy/golucene/core_test
+go test github.com/balzaczyy/golucene/core_test
